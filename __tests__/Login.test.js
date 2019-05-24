0d3e6750fee779463f6a31405641b07013550a0b
jonathanmusila <jonathanmusila6@gmail.com>
2019-05-24 11:54:44 +0300

    added more tests

--- __tests__/Login.test.js
+++ __tests__/Login.test.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import LoginComponent from '../src/components/signin';
-import { wrap } from 'module';
 
 it('should test onChange function', () => {
     const event = {
@@ -17,6 +16,23 @@ it('should test onChange function', () => {
         .find('#password')
         .simulate('change', event);
     expect(wrapper.state().loginUser.password).toBe('hfklfhhsfhdf');
+    expect(wrapper.length).toBe(1);
+});
+
+
+it('should test onChange function email', () => {
+    const event = {
+        target: {
+            value: 'meme@andela.com'
+        }
+    }
+    const wrapper = shallow(<LoginComponent />);
+    wrapper.signinUser;
+    wrapper.find('Form')
+        .dive()
+        .find('#email')
+        .simulate('change', event);
+    expect(wrapper.state().loginUser.email).toBe('meme@andela.com');
     expect(wrapper.length).toBe(1);
 });
 
--- __tests__/Register.test.js
+++ __tests__/Register.test.js
@@ -18,6 +18,71 @@ it('should test onChange function', () => {
     expect(wrapper.length).toBe(1);
 });
 
+it('should test onChange function', () => {
+    const event = {
+        target: {
+            value: 'hfklfhhsfhdf'
+        }
+    }
+    const wrapper = shallow(<RegisterComponent />);
+    wrapper.addUser;
+    wrapper.find('Form')
+        .dive()
+        .find('#lastname')
+        .simulate('change', event);
+    expect(wrapper.state().newUserData.lastname).toBe('hfklfhhsfhdf');
+    expect(wrapper.length).toBe(1);
+});
+
+it('should test onChange function', () => {
+    const event = {
+        target: {
+            value: 'hfklfhhsfhdf'
+        }
+    }
+    const wrapper = shallow(<RegisterComponent />);
+    wrapper.addUser;
+    wrapper.find('Form')
+        .dive()
+        .find('#username')
+        .simulate('change', event);
+    expect(wrapper.state().newUserData.username).toBe('hfklfhhsfhdf');
+    expect(wrapper.length).toBe(1);
+});
+
+it('should test onChange function', () => {
+    const event = {
+        target: {
+            value: 'mme@andela.com'
+        }
+    }
+    const wrapper = shallow(<RegisterComponent />);
+    wrapper.addUser;
+    wrapper.find('Form')
+        .dive()
+        .find('#email')
+        .simulate('change', event);
+    expect(wrapper.state().newUserData.email).toBe('mme@andela.com');
+    expect(wrapper.length).toBe(1);
+});
+
+it('should test onChange function', () => {
+    const event = {
+        target: {
+            value: 'hhhhvjfhefv'
+        }
+    }
+    const wrapper = shallow(<RegisterComponent />);
+    wrapper.addUser;
+    wrapper.find('Form')
+        .dive()
+        .find('#password')
+        .simulate('change', event);
+    expect(wrapper.state().newUserData.password).toBe('hhhhvjfhefv');
+    expect(wrapper.length).toBe(1);
+});
+
+
 it('should test onClick function', () => {
     const state = {
         newUserData: {
