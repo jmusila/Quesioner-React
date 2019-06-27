bdb7b80fbe6d6700f0854676d3d243a85b907b97
jonathanmusila <jonathanmusila6@gmail.com>
2019-06-27 15:17:54 +0300

    added more tests

---
+++ src/reducers/__tests__/home.test.js
@@ -0,0 +1,20 @@
+import homeReducer, { initialState } from '../homeReducer';
+import * as types from '../../constants/homeTypes';
+
+describe('Fetch meetups reducer tests', () => {
+    it('should have the initial state', () => {
+        expect(homeReducer(initialState, {})).toBe(initialState);
+    });
+    it('should show loading when FETCH_MEETUPS is dispatched', () => {
+        expect(homeReducer(initialState,
+            { type: types.FETCH_MEETUPS }).loading).toBe(true);
+    });
+    it('should set loading to false when FETCH_SUCCESS is dispatched', () => {
+        expect(homeReducer(initialState,
+            { type: types.FETCH_SUCCESS, payload: {} }).loading).toBe(false);
+    });
+    it('should set loading to false when FETCH_ERROR is dispatched', () => {
+        expect(homeReducer(initialState,
+            { type: types.FETCH_ERROR, payload: {} }).loading).toBe(false);
+    });
+})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
 import homeReducer from './homeReducer';
 import loginReducer from './loginReducer';
+import registerReducer from './registerReducer';
 
 
 export default combineReducers({
     meetups: homeReducer,
-    login: loginReducer
+    login: loginReducer,
+    register: registerReducer,
+
 });
